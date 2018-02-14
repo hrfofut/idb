69ac1f5e553d875276304278ce3dc4c35d850338
straitlaced <straitlaced@keemail.me>
2018-02-13 21:40:58 -0600

    foods.py returns valid page for id=0-2, error otherwise

--- idb/views/foods.py
+++ idb/views/foods.py
@@ -2,15 +2,21 @@ from flask import Blueprint, render_template
 
 foods = Blueprint('foods', __name__)
 
+first = 0
+last = 2
+
 
 @foods.route("/")
 def foods_overview():
     return render_template('foods/food.html')
 
 
-@foods.route("/<id>")
+@foods.route("/<int:id>")
 def food_detail(id):
     """TODO: Have the template be filled from a database in the future"""
     # However the backend will work, use id to find all the info.
 
+    # ID 1-3 returns certain food page, else return error
+    if id < first or id > last:
+        return render_template('errors/404.html'), 404
     return render_template('foods/fooddetail.html', title=id)
