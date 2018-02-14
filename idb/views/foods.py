eb40b095f37aa5eb68f8a3167e8e83d79632dc14
Harrison Foreman <harrisonrforeman@gmail.com>
2018-02-14 14:34:08 -0600

    Added dynamic model pages, only food works right now, every other model page will error.

--- idb/views/foods.py
+++ idb/views/foods.py
@@ -8,7 +8,14 @@ last = 2
 
 @foods.route("/")
 def foods_overview():
-    return render_template('foods/food.html')
+    icecream = {'img': 'http://del.h-cdn.co/assets/17/23/1600x800/landscape-1497238977-delish-mason-jar-ice-cream-3.jpg', 'name': 'ice cream', 'calories': '137', 'serving': '1/2 cup', 'sodium': '53 mg', 'fat': '7 g', 'protein': '2.3 g'}
+    burger = {'img': 'http://baileysrestaurants.com/range/files/2013/02/IMG_1700.jpg', 'name': 'burger', 'calories': '354', 'serving': '1 item', 'sodium': '497 mg', 'fat': '17 g', 'protein': '20 g'}
+    waffle = {'img': 'http://catchyscoop.com/wp-content/uploads/2016/07/calories-in-a-waffle.jpg', 'name': 'waffle', 'calories': '82', 'serving': '1 oz', 'sodium': '145 mg', 'fat': '4 g', 'protein': '2.2 g'}
+    foods = [icecream, burger, waffle]
+    # TODO: I need to remember to describe whats going on later.
+    items = [] for val in foods:
+        items.append([val['name'], val['img'], val['calories'], val['fat']])
+    return render_template('foods/food.html', items=items)
 
 
 @foods.route("/<int:id>")
@@ -22,6 +29,7 @@ def food_detail(id):
     waffle = {'img': 'http://catchyscoop.com/wp-content/uploads/2016/07/calories-in-a-waffle.jpg', 'name': 'waffle', 'calories': '82', 'serving': '1 oz', 'sodium': '145 mg', 'fat': '4 g', 'protein': '2.2 g'}
 
     foods = [icecream, burger, waffle]
+
     if id < first or id > last:
         return render_template('errors/404.html'), 404
     return render_template('foods/fooddetail.html', food=foods[id])
