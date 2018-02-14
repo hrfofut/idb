9290a7d9db36020316496bf523b6089f1d790ccb
straitlaced <straitlaced@keemail.me>
2018-02-13 22:21:34 -0600

    yep

--- idb/views/workouts.py
+++ idb/views/workouts.py
@@ -2,13 +2,20 @@ from flask import Blueprint, render_template
 
 workouts = Blueprint('workouts', __name__)
 
+first = 0
+last = 2
+
 
 @workouts.route("/")
 def workouts_overview():
     return render_template('workouts/workouts.html')
 
 
-@workouts.route("/<id>")
+@workouts.route("/<int:id>")
 def workouts_detail(id):
     """TODO: Have the template be filled from a database in the future"""
+
+    # ID 0-2 returns certain food page, else return error
+    if id < first or id > last:
+        return render_template('errors/404.html'), 404
     return render_template('workouts/workoutsdetail.html', title=id)
