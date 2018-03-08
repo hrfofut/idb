8c2a983561220daf4655014530a2823030163133
straitlaced <straitlaced@keemail.me>
2018-03-08 13:32:15 -0600

    Fixes #65; Github API call bug fixed, also gets all issues, not just first 500

--- idb/views/general.py
+++ idb/views/general.py
@@ -13,16 +13,6 @@ def splash(name=None):
 
 @general.route("/about")
 def about():
-    logins = {'cindyqtruong': 0, 'BrandonHarrisonCode': 1, 'hrfofut': 2, 'straitlaced': 3, 'fantomats15': 4}
-    iss_count = []
-    iss_total = 0
-    for i in range(0, 5):
-        iss_count.append(0)
-    req_issue = requests.get('https://api.github.com/repos/hrfofut/idb/issues?state=all&page=1&per_page=500')
-    for issue in (req_issue.json()):
-        iss_total += 1
-        iss_count[logins[issue['user']['login']]] += 1
-    iss_count.append(iss_total)
 
     # get commits
     commit_count = {'total': 0, 'cindyqtruong': 0, 'BrandonHarrisonCode': 0, 'hrfofut': 0, 'straitlaced': 0, 'fantomats15': 0}
@@ -35,7 +25,19 @@ def about():
                 commit_count['total'] += cont['total']
 
     # get issues
-
+    logins = {'cindyqtruong': 0, 'BrandonHarrisonCode': 1, 'hrfofut': 2, 'straitlaced': 3, 'fantomats15': 4}
+    iss_count = [0, 0, 0, 0, 0]
+    iss_total = 0
+    i = 1
+    req_issues = requests.get('https://api.github.com/repos/hrfofut/idb/issues?state=all&page=' + str(i) + '&per_page=500')
+    while (req_issues.status_code == requests.codes.ok) and (len(req_issues.json()) != 0):
+        req_issues_json = req_issues.json()
+        for issue in req_issues_json:
+            iss_total += 1
+            iss_count[logins[issue['user']['login']]] += 1
+        i += 1
+        req_issues = requests.get('https://api.github.com/repos/hrfofut/idb/issues?state=all&page=' + str(i) + '&per_page=500')
+    iss_count.append(iss_total)
     return render_template('about.html', commits=commit_count, issues=iss_count)
 
 # Error handling
