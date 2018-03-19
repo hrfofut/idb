088a6923d6d54f91c46b44818a2651e15a55a5e3
Gia Bao Than <fantomats15@yahoo.com>
2018-03-18 19:48:01 -0500

    Added database.py, although not working yet

---
+++ idb/database.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql + psycopg2:
+    //Calorieking:
+        seargohsiEHFGUOAEHRGIOHPOSk578@ckcdbinstance.c48dginqtauy.us - west - 2.rds.amazonaws.com / CKCDB
+
+db = SQLAlchemy(app)
