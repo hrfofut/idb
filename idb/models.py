5d61e2798733055c1e334d410225c7a77e9ff875
Gia Bao Than <fantomats15@yahoo.com>
2018-03-20 01:24:04 -0500

    Trying to add stores table

--- idb/models.py
+++ idb/models.py
@@ -11,3 +11,20 @@ class Food(db.Model):
 
     def __repr__(self):
         return '<Name %r>' % self.name
+
+
+class Stores(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(80), nullable=False)
+    location = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(255))
+    phone = db.Column(db.Integer)
+    ratings = db.Column(db.Integer)
+
+    def __init__(self, id, name, location, description, phone, ratings)
+        self.id = id
+        self.name = name
+        self.location = location
+        self.description = description
+        self.phone = phone
+        self.ratings = ratings
