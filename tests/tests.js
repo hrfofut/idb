89f0fcd39451868737c80f432b4e7dd4d852039c
Cindy Truong <cindy.q.truong@gmail.com>
2018-03-22 21:47:32 -0500

    Mocha planned test

---
+++ tests/tests.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme'
+
+import { FoodDetail } from '../idb/static/js/fooddetail.js';
+
+describe('FoodDetail', function() {
+  it('should have props for name, servings, calorie, sodium, fat, and protein', function () {
+    
+  });
+
+  it('should have a title', function () {
+
+  });
+
+  it('should render a table', function () {
+
+  });
+
+  it('test' function() {
+    assert.equal(1, 1);
+  });
+});
