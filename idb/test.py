33eca5b920f0e6cb85a6d6a86e773bed1da9d62c
Wesley Joe <wjj338@cs.utexas.edu>
2018-04-04 17:56:20 -0500

    Start unit testing general.py

---
+++ idb/test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+
+# ---------------------
+# backend website tests
+# ---------------------
+
+# https://docs.python.org/3.4/reference/simple_stmts.html#grammar-token-assert_stmt
+
+# -------
+# imports
+# -------
+
+from io import StringIO
+from unittest import main, TestCase
+from views.general import splash, search, about
+
+# -------
+# TestWeb
+# -------
+
+
+class TestWeb (TestCase):
+
+    def test_test(self):
+        self.assertEqual(1, 1)
+
+# ----
+# main
+# ----
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
