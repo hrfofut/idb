8072e008055cd155f1be3d2e9d983425eb5224f7
Cindy Truong <cindy.q.truong@gmail.com>
2018-04-04 11:05:47 -0500

    Fix search so that it's case insensitive

--- idb/views/general.py
+++ idb/views/general.py
@@ -27,25 +27,25 @@ def search():
 
     for search in tokens:
         print("search: {}".format(search))
-        get_foods = db.session.query(Food).filter(Food.name.like("%" + search + "%")).all()
+        get_foods = db.session.query(Food).filter(Food.name.ilike("%" + search + "%")).all()
         for val in get_foods:
             image = 'https://spoonacular.com/cdn/ingredients_500x500/' + val.img
             if ([val.name.title(), image, val.calorie, val.fat, val.id]) not in food_items:
                 food_items.append([val.name.title(), image, val.calorie, val.fat, val.id])
 
-        get_workouts = db.session.query(Workouts).filter(Workouts.name.like("%" + search + "%")).all()
+        get_workouts = db.session.query(Workouts).filter(Workouts.name.ilike("%" + search + "%")).all()
         for val in get_workouts:
             if val.name != "" and ([val.name, val.img, val.category, val.muscle, val.id]) not in workout_items:
                 workout_items.append([val.name, val.img, val.category, val.muscle, val.id])
 
-        get_gyms = db.session.query(Gyms).filter(Gyms.name.like("%" + search + "%")).all()
+        get_gyms = db.session.query(Gyms).filter(Gyms.name.ilike("%" + search + "%")).all()
         for gym in get_gyms:
             image = db.session.query(Images).get(gym.pic_id).pic
             img = unbinary(str(base64.b64encode(image)))
             if ([gym.name, img, gym.location, gym.ratings, gym.id]) not in gym_items:
                 gym_items.append([gym.name, img, gym.location, gym.ratings, gym.id])
 
-        get_stores = db.session.query(Stores).filter(Stores.name.like("%" + search + "%")).all()
+        get_stores = db.session.query(Stores).filter(Stores.name.ilike("%" + search + "%")).all()
         for store in get_stores:
             image = db.session.query(Images).get(store.pic_id).pic
             img = unbinary(str(base64.b64encode(image)))
